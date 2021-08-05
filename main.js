f08b54675b8d6d254695963a4c0b2452dd7c389e
bappurii <bappul_tt@naver.com>
2021-08-05 22:40:00 +0900

    chatbot start

--- main.js
+++ main.js
@@ -4,19 +4,22 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded( {extended : false } ));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.listen(3000);
+// app.post('/message', (req, res) => {
+//     const question = req.body.userRequest.utterance;
+//     let data={
 
-app.post('/message', (req, res) => {
-    const question = req.body.userRequest.utterance;
-    let data={
+//         'type' : 'buttons',
 
-        'type' : 'buttons',
+//         'buttons' : ['도움말','장학금','현장실습']
 
-        'buttons' : ['도움말','장학금','현장실습']
+//     };
 
-    };
+//     // json 형식으로 응답
 
-    // json 형식으로 응답
+//     res.json(data);
 
-    res.json(data);
-
-});
+// });
