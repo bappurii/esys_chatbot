ae4eaf9d8c74bf2074a4799e39dd822f3cd1db9d
bappurii <bappul_tt@naver.com>
2021-08-11 23:30:59 +0900

    profList test

--- apiRouter/profList.js
+++ apiRouter/profList.js
@@ -18,46 +18,118 @@ const cn = mysql.createConnection({
 
 
 router.post('/machine',(req,res)=>{
-    cn.query(`select * from prof`,(err, result)=>{
-        if(err) console.log(err);
-        let arr = [];
-        for(let i=0; i<result.length; i++){
-            arr[i]={
-                "title": result[i].name,
-                "description": result[i].depart,
-                "thumbnail":{
-                    "imageUrl": result[i].image
-                },
-                "buttons": [
+    // cn.query(`select * from prof`,(err, result)=>{
+    //     if(err) console.log(err);
+    //     let arr = [];
+    //     for(let i=0; i<result.length; i++){
+    //         arr[i]={
+    //             "title": result[i].name,
+    //             "description": result[i].depart,
+    //             "thumbnail":{
+    //                 "imageUrl": result[i].image
+    //             },
+    //             "buttons": [
+    //                 {
+    //                     "action":  "webLink",
+    //                     "label": "자세한 프로필 링크",
+    //                     "webLinkUrl": result[i].profile
+    //                 },
+    //                 {
+    //                     "action":  "webLink",
+    //                     "label": "연구실 링크",
+    //                     "webLinkUrl": result[i].lab
+    //                 }
+    //             ]
+    //         }
+    //     }
+    //     console.log(arr);
+    //     const responseBody = {
+    //         "version": "2.0",
+    //         "template": {
+    //             "outputs": [
+    //             {
+    //                 "carousel": {
+    //                 "type": "basicCard",
+    //                 "items": arr
+    //                 }
+    //             }
+    //             ]
+    //         }
+    //     }
+    //     res.status(200).send(responseBody);
+    // })
+    const responseBody={
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                "carousel": {
+                    "type": "basicCard",
+                    "items": [
                     {
-                        "action":  "webLink",
-                        "label": "자세한 프로필 링크",
-                        "webLinkUrl": result[i].profile
+                        "title": "보물상자",
+                        "description": "보물상자 안에는 뭐가 있을까",
+                        "thumbnail": {
+                        "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
+                        },
+                        "buttons": [
+                        {
+                            "action": "message",
+                            "label": "열어보기",
+                            "messageText": "짜잔! 우리가 찾던 보물입니다"
+                        },
+                        {
+                            "action":  "webLink",
+                            "label": "구경하기",
+                            "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                        }
+                        ]
                     },
                     {
-                        "action":  "webLink",
-                        "label": "연구실 링크",
-                        "webLinkUrl": result[i].lab
+                        "title": "보물상자2",
+                        "description": "보물상자2 안에는 뭐가 있을까",
+                        "thumbnail": {
+                        "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
+                        },
+                        "buttons": [
+                        {
+                            "action": "message",
+                            "label": "열어보기",
+                            "messageText": "짜잔! 우리가 찾던 보물입니다"
+                        },
+                        {
+                            "action":  "webLink",
+                            "label": "구경하기",
+                            "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                        }
+                        ]
+                    },
+                    {
+                        "title": "보물상자3",
+                        "description": "보물상자3 안에는 뭐가 있을까",
+                        "thumbnail": {
+                        "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
+                        },
+                        "buttons": [
+                        {
+                            "action": "message",
+                            "label": "열어보기",
+                            "messageText": "짜잔! 우리가 찾던 보물입니다"
+                        },
+                        {
+                            "action":  "webLink",
+                            "label": "구경하기",
+                            "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                        }
+                        ]
                     }
-                ]
-            }
-        }
-        console.log(arr);
-        const responseBody = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                {
-                    "carousel": {
-                    "type": "basicCard",
-                    "items": arr
-                    }
+                    ]
                 }
-                ]
+                }
+            ]
             }
         }
         res.status(200).send(responseBody);
-    })
 })
 
 
