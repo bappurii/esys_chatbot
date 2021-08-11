ff5ea26ea755a69fc163f41b1e762ffea0baa3c2
bappurii <bappul_tt@naver.com>
2021-08-12 00:42:43 +0900

    test9

--- apiRouter/profList.js
+++ apiRouter/profList.js
@@ -16,9 +16,9 @@ const cn = mysql.createConnection({
     database : 'esys_mailer'
 });
 
-cn.query(`select * from prof where id=1`,(err, result)=>{
-    router.post('/machine',(req,res)=>{
 
+router.post('/machine',(req,res)=>{
+    cn.query(`select * from prof where id=1`,(err, result)=>{
         if(err) console.log(err);
         // let arr = [];
         // arr[0]={
@@ -86,25 +86,25 @@ cn.query(`select * from prof where id=1`,(err, result)=>{
                     "carousel": {
                         "type": "basicCard",
                         "items": [
-                        {
-                            "title": result[0].name,
-                            "description": result[0].lecture,
-                            "thumbnail": {
-                            "imageUrl": result[0].image
-                            },
-                            "buttons": [
-                            {
-                                "action": "webLink",
-                                "label": "profile",
-                                "messageText": result[0].profile
-                            },
-                            {
-                                "action":  "webLink",
-                                "label": "lab",
-                                "webLinkUrl": result[0].lab
-                            }
-                            ]
-                        },
+                        // {
+                        //     "title": result[0].name,
+                        //     "description": result[0].lecture,
+                        //     "thumbnail": {
+                        //     "imageUrl": result[0].image
+                        //     },
+                        //     "buttons": [
+                        //     {
+                        //         "action": "webLink",
+                        //         "label": "profile",
+                        //         "messageText": result[0].profile
+                        //     },
+                        //     {
+                        //         "action":  "webLink",
+                        //         "label": "lab",
+                        //         "webLinkUrl": result[0].lab
+                        //     }
+                        //     ]
+                        // },
                         {
                             "title": "보물상자2",
                             "description": "보물상자2 안에는 뭐가 있을까",
